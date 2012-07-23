6b57d0da91ff83dc077482093cddb5ccb6e2a1ae
stephenball1 <stephenball1@gmail.com>
2012-07-23 14:35:06 -0400

    More UI work. Working on default screen

--- gen/com/viasat/R.java
+++ gen/com/viasat/R.java
@@ -14,14 +14,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050002;
-        public static final int editText1=0x7f050000;
-        public static final int editText2=0x7f050001;
+        public static final int GridLayout1=0x7f050000;
+        public static final int editText1=0x7f050001;
+        public static final int editText2=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int FlightDate=0x7f040003;
+        public static final int FlightNumber=0x7f040002;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
     }
