c70833efc6259c032743eb82ddc24d65e4f8dacc
Stephen Pinto <stephen.pinto42@gmail.com>
2012-07-23 18:50:11 -0700

    Added movie watching support

---
+++ src/com/viasat/WatchMovie.java
@@ -0,0 +1,35 @@
+package com.viasat;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class WatchMovie extends Activity {
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.watchmovie);
+        
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        
+        VideoView videoView = (VideoView)findViewById(R.id.movieView);
+        MediaController mc = new MediaController(this);
+        videoView.setMediaController(mc);
+
+        
+        String str = "http://10.11.90.17/movie.mp4";
+        Uri uri = Uri.parse(str);
+
+        videoView.setVideoURI(uri);
+
+        videoView.requestFocus();
+        videoView.start();
+    }
+
+}
