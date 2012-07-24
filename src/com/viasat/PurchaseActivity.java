b3f4c1cabfa05e2105cc079c8cf2af8d5ff10e0f
stephenball1 <stephenball1@gmail.com>
2012-07-24 03:26:35 -0400

    added purchase activity placeholder

---
+++ src/com/viasat/PurchaseActivity.java
@@ -0,0 +1,14 @@
+package com.viasat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PurchaseActivity extends Activity{
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.standard);
+	}
+}
